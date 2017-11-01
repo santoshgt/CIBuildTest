2fa1ac7b8b2da49af65e5c4f92d64a2a64432395
Santosh <stimilsina@braindigit.com>
2017-11-01 17:51:52 +0545

    CITest project added

---
+++ CITest/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using CITestBuild;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CITest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<UserInfo> lstUsers = new List<UserInfo>();
+            CIBuildTest objTest = new CIBuildTest();
+            lstUsers = objTest.GetUserList();
+            grdUsers.AutoGenerateColumns = true;
+            grdUsers.DataSource = lstUsers;
+            grdUsers.DataBind();
+        }
+    }
+}
---
+++ CITestBuild/CIBuildTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITestBuild
+{
+    public class CIBuildTest
+    {
+        public CIBuildTest()
+        {
+
+        }
+
+        public List<UserInfo> GetUserList()
+        {
+            return new List<UserInfo> { new UserInfo { UserID = 1, Name = "Pratap", Email = "pratap@yopmail.com" },
+                new UserInfo { UserID = 1, Name = "santosh", Email = "santosh@yopmail.com" } };
+        }
+
+    }
+}
