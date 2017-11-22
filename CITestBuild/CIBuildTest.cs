fcff6c2388abb74431805bf5d5aae220a30177b9
Santosh <stimilsina@braindigit.com>
2017-11-22 17:58:31 +0545

    changed

--- CITestBuild/CIBuildTest.cs
+++ CITestBuild/CIBuildTest.cs
@@ -16,7 +16,7 @@ namespace CITestBuild
         public List<UserInfo> GetUserList()
         {
             return new List<UserInfo> { new UserInfo { UserID = 1, Name = "Pratap", Email = "pratap@yopmail.com" },
-                new UserInfo { UserID = 2, Name = "santosh", Email = "santosh@yopmail.com" }, new UserInfo { UserID = 3, Name = "prabhash", Email = "pravash@yopmail.com" },new UserInfo { UserID = 4, Name = "swornim", Email = "swornim@yopmail.com" } ,new UserInfo { UserID = 5, Name = "prakash", Email = "prakash@yopmail.com" }};
+                new UserInfo { UserID = 2, Name = "santosh", Email = "santosh@yopmail.com" }, new UserInfo { UserID = 3, Name = "prabhash", Email = "pravash@yopmail.com" },new UserInfo { UserID = 4, Name = "swornim", Email = "swornim@yopmail.com" } ,new UserInfo { UserID = 6, Name = "prakash", Email = "prakash@yopmail.com" }};
         }
 
     }
